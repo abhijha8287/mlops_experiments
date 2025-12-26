71633b4ded67294c6eccdd817a90c7f4df0898bc
Abhishek Jha <abhijha8287@gmail.com>
2025-12-26 14:31:58 +0530

    learning how to use dagshub

---
+++ src/file2.py
@@ -0,0 +1,61 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+# mlflow.set_tracking_uri("http://localhost:5000")
+import dagshub
+dagshub.init(repo="mlops_experiments", owner="abhijha", mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/abhijha8287/mlops_experiments.mlflow")
+
+
+wine=load_wine()
+x=wine.data
+y=wine.target
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+max_depth = 20
+n_estimators = 70
+
+mlflow.set_experiment("First ML experiments")
+
+with mlflow.start_run():
+    clf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
+    clf.fit(x_train, y_train)
+    
+    y_pred = clf.predict(x_test)
+    
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred, target_names=wine.target_names)
+    cm = confusion_matrix(y_test, y_pred)
+    
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_metric("accuracy", accuracy)
+
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(8,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names)
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title('Confusion Matrix')
+
+    plt.savefig("confusion_matrix.png")
+    mlflow.log_artifact("confusion_matrix.png")
+    mlflow.log_artifact(__file__)
+    
+    mlflow.set_tags({
+    "author": "abhijha",
+    "model": "RandomForestClassifier"
+})
+
+
+    mlflow.sklearn.log_model(clf, "random_forest_model")
+
+    print("Classification Report:\n", report)
+    
